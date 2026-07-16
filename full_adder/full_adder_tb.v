ecb69148f73a7ab5cd2bd47e2b777e9ae48947e7
Swathi <swathi37384@gmail.com>
2026-07-16 13:10:02 +0000

    full adder

--- full_adder/full_adder_tb.v
+++ full_adder/full_adder_tb.v
@@ -1,1 +1,17 @@
-
+module full_adder_tb;
+reg a,b,c;
+wire sum,carry;
+full_adder uut(.a(a),.b(b),.c(c),.sum(sum),.carry(carry));
+initial begin
+$monitor(" time=%0t a=%b b=%b c=%b sum=%b carry=%b", $time,a,b,c,sum,carry);
+a=0;b=0;c=0;#10;
+a=0;b=0;c=1;#10;
+a=0;b=1;c=0;#10;
+a=0;b=1;c=1;#10;
+a=1;b=0;c=0;#10;
+a=1;b=0;c=1;#10;
+a=1;b=1;c=0;#10;
+a=1;b=1;c=1;#10;
+$finish;
+end 
+endmodule
