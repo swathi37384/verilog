bc4c59ae4cebf872fc93b79a9312cdec659a1d28
Swathi <swathi37384@gmail.com>
2026-07-17 17:35:17 +0000

    function is updated

--- assignment/alu_fun/alu.v
+++ assignment/alu_fun/alu.v
@@ -7,28 +7,28 @@ assign dif=sub(a,b);
 assign mul=pro(a,b);
 assign div=divide(a,b);
 function [4:0] add;
-input [3:0]x,y;
-begin
-add=x+y;
-end
+	input [3:0]x,y;
+	begin
+	add=x+y;
+	end
 endfunction 
 function [4:0] sub;
-input [3:0]x,y;
-begin
-sub=x-y;
-end
+	input [3:0]x,y;
+	begin
+	sub=x-y;
+	end
 endfunction
 function [7:0] pro;
-input [3:0]x,y;
-begin
-pro=x*y;
-end
+	input [3:0]x,y;
+	begin
+	pro=x*y;
+	end
 endfunction
 function [3:0] divide;
-input [3:0]x,y;
-begin
-divide=x/y;
-end
+	input [3:0]x,y;
+	begin
+	divide=x/y;
+	end
 endfunction
 endmodule 
 
