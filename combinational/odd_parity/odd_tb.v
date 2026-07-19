420e9228c5e285ff3924c50006ffe439e5174b28
Swathi <swathi37384@gmail.com>
2026-07-19 17:57:03 +0000

    odd parity

---
+++ combinational/odd_parity/odd_tb.v
@@ -0,0 +1,17 @@
+module odd_tb;
+reg [2:0]a;
+wire p;
+odd uut(.a(a),.p(p));
+initial begin
+$monitor("a=%b |p=%b",a,p);
+a=3'b000;#10;
+a=3'b001;#10; 
+a=3'b010;#10; 
+a=3'b011;#10; 
+a=3'b100;#10; 
+a=3'b101;#10;
+a=3'b110;#10; 
+a=3'b111;#10;
+$finish;
+end 
+endmodule  
