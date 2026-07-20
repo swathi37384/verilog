428e92eb76fadd39018d4fb91f99a01c7b99578f
Swathi <swathi37384@gmail.com>
2026-07-20 17:21:07 +0000

    ques 4 updated

---
+++ hdl_bits/kmap/ques_4_tb.v
@@ -0,0 +1,25 @@
+module k_2_tb;
+reg a,b,c,d;
+wire out;
+k_2 uut(.a(a),.b(b),.c(c),.d(d),.out(out));
+initial begin
+$monitor("a=%b b=%b c=%b d=%d out=%b",a,b,c,d,out);
+a=0;b=0;c=0;d=0;#10;
+a=0;b=0;c=0;d=1;#10; 
+a=0;b=0;c=1;d=0;#10; 
+a=0;b=0;c=1;d=1;#10; 
+a=0;b=1;c=0;d=0;#10; 
+a=0;b=1;c=0;d=1;#10; 
+a=0;b=1;c=1;d=0;#10; 
+a=0;b=1;c=1;d=1;#10; 
+a=1;b=0;c=0;d=0;#10; 
+a=1;b=0;c=0;d=1;#10; 
+a=1;b=0;c=1;d=0;#10; 
+a=1;b=0;c=1;d=1;#10; 
+a=1;b=1;c=0;d=0;#10; 
+a=1;b=1;c=0;d=1;#10; 
+a=1;b=1;c=1;d=0;#10; 
+a=1;b=1;c=1;d=1;#10; 
+$finish;
+end
+endmodule
