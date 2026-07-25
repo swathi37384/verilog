b8a7fa25be1e3b6b0a4db3eaa79aa6b52ccea0d2
Swathi <swathi37384@gmail.com>
2026-07-25 17:45:43 +0000

    updated

--- combinational/dec_to_bcd_encoder/dec_bcd_tb.v
+++ combinational/dec_to_bcd_encoder/dec_bcd_tb.v
@@ -1,15 +1,14 @@
 module dec_bcd_tb;
-reg [3:0]d;
+reg [9:0]d;
 wire [3:0]out;
-integer i;
 dec_bcd uut(.d(d),.out(out));
 initial begin
-$monitor("d=%0d bcd=%b",d,out);
-for(i=0;i<11;i=i+1)
-begin
-d=i;
-#10;
-end
+$monitor("d=%b bcd=%b",d,out);
+d=10'b0000000001;#10;
+d=10'b0000000100;#10;
+d=10'b0010000000;#10;
+d=10'b0000010000;#10;
+d=10'b0000010001;#10;
 #10;$finish;
 end
 endmodule
