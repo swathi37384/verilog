a13588ed419b7bf164dfaa3496b74c379864ac0d
Swathi <swathi37384@gmail.com>
2026-07-27 13:16:34 +0000

    sr to jk flip flop

---
+++ sequential_ckt/flip_flop/sr_to_jk/sr_to_jk.v
@@ -0,0 +1,21 @@
+module sr_to_jk(input j,k, clk,rst,
+output reg q);
+wire s,r;
+assign s=j;
+assign r=k;
+always@(posedge clk or negedge rst)begin
+if(!rst)
+	q<=1'b0;
+else
+begin
+	case({s,r})
+		2'b00 :q<=q;
+		2'b01 :q<=1'b0;
+		2'b10 :q<=1'b1;
+		2'b11 :q<=1'bx;
+	endcase
+end
+end
+endmodule
+
+
---
+++ sequential_ckt/flip_flop/sr_to_jk/sr_to_jk_tb.v
@@ -0,0 +1,17 @@
+module sr_to_jk_tb;
+reg clk,rst,j,k;
+wire q;
+sr_to_jk uut(.clk(clk),.rst(rst),.j(j),.k(k),.q(q));
+always #5 clk=~clk;
+initial begin
+$monitor("rst=%b clk=%b j=%b k=%b q=%b",rst,clk,j,k,q);
+clk=0;
+rst=0;j=0;k=0;
+#10 rst=1;
+j=0;k=0;#10;
+j=0;k=1;#10;
+j=1;k=0;#10;
+j=1;k=1;#10;
+$finish;
+end
+endmodule
