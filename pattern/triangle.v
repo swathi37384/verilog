81343c0c392c3e8dec63980cfa47209e3eb29c8f
Swathi <swathi37384@gmail.com>
2026-08-06 04:42:48 +0000

    triangle pattern

---
+++ pattern/triangle.v
@@ -0,0 +1,15 @@
+module triangle;
+integer i,j,k,n;
+initial begin
+n=5;
+for(i=1;i<=n;i=i+1)begin
+	for(j=1;j<=n-i;j=j+1)begin
+		$write(" ");
+	end
+	for(k=1;k<=i;k=k+1)begin
+		$write("* ");
+	end
+	$display("");
+end
+end
+endmodule
