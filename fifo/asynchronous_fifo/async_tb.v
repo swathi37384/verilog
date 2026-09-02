a659924123daa3d2309e20e929b4729f2b62e24c
Swathi <swathi37384@gmail.com>
2026-09-02 04:29:30 +0000

    fifo

---
+++ fifo/asynchronous_fifo/async_tb.v
@@ -0,0 +1,77 @@
+`timescale 1ns/1ps
+
+module async_fifo_tb;
+reg wclk,rclk;
+reg w_rst,r_rst;
+reg w_en,r_en;
+reg [7:0]wdata;
+wire [7:0]rdata;
+wire wfull;
+wire rempty;
+
+async_fifo uut(.wclk(wclk),
+	.w_rst(w_rst),
+	.w_en(w_en),
+	.wdata(wdata),
+	.rclk(rclk),
+	.r_rst(r_rst),
+	.r_en(r_en),
+	.rdata(rdata),
+	.wfull(wfull),
+	.rempty(rempty));
+
+always #5 wclk=~wclk;
+always #10 rck=~rclk;
+
+initial begin
+	wclk=0;
+	rclk=0;
+	w_rst=0;
+	r_rst=0;
+	w_en=0;
+	r_en=0;
+	wdata=8'b00000000;
+
+	#20;
+	w_rst=1;
+	r_rst=1;
+
+	#20;
+	w_en=1;
+	wdata=8'h11;#10;
+	wdata=8'h22;#10;
+	wdata=8'h33;#10;
+	wdata=8'h44;#10;
+	wdata=8'h55;#10;
+	wdata=8'h66;#10;
+	wdata=8'h77;#10;
+	wdata=8'h88;#10;
+
+	w_en=0;#30;
+	r_en=1;
+	#20;
+	#20;
+	#20;
+	#20;
+	#20;
+	#20;
+	#20;
+	#20;
+	r_en=0;
+	#50;
+	$finish;
+end
+
+always@(posedge wclk) begin
+	$display("time=%0t w_e=%b wdata=%h full=%b",$time,w_en,wdata,wfull);
+end
+
+always@(posedge rclk) begin
+        $display("time=%0t r_e=%b rdata=%h empty=%b",$time,r_en,rdata,rempty);
+end
+
+initial begin 
+	$dumpfile("async_fifo.vcd");
+	$dumpvars;
+end
+endmodule
---
+++ fifo/asynchronous_fifo/design_code.v
@@ -0,0 +1,185 @@
+//memory block
+module memory(input wclk,rclk,
+input w_en,r_en,
+input [2:0]w_add,r_add,
+input [7:0]wdata,
+output reg [7:0]rdata);
+
+reg [7:0]mem[0:7];
+
+always@(posedge wclk)begin
+if(w_en)
+	mem[w_add]<=wdata;
+end
+
+always@(posedge rclk)begin
+if(r_en)
+	rdata<=mem[r_add];
+end
+
+endmodule
+
+
+//write pointer handler
+
+module write_handler(input wclk,w_rst,
+	input w_en,
+	input [3:0]r_gray_sync,
+	output reg [3:0]w_bi_ptr,
+	output reg [3:0]w_gray_ptr,
+	output reg wfull);
+
+reg [3:0]r_bi_sync;
+
+always@(*)begin
+r_bi_sync[3]=r_gray_sync[3];
+r_bi_sync[2]=r_bi_sync[3]^r_gray_sync[2];
+r_bi_sync[1]=r_bi_sync[2]^r_gray_sync[1];
+r_bi_sync[2]=r_bi_sync[1]^r_gray_sync[0];
+end
+
+always@(posedge wclk or negedge w_rst)begin
+	if(!w_rst)begin
+		w_bi_ptr<=4'd0;
+		w_gray_ptr<=4'd0;
+		wfull<=1'b0;
+	end
+	else begin
+		if(w_en && !wfull) begin
+			w_bi_ptr<=w_bi_ptr+1;
+			w_gray_ptr<=((w_bi_ptr+1)>>1)^(w_bi_ptr+1);
+		end
+		if((w_bi_ptr[2:0]==r_bi_sync[2:0])&&(w_bi_ptr[3]!=r_bi_sync[3])) begin
+			wfull<=1'b1;
+		end
+		else begin
+		       wfull<=1'b0;
+	       end
+       end
+end
+endmodule
+
+// read pointer handler
+
+module read_handler(input rclk,
+	input r_en,
+	input r_rst,
+	input [3:0]w_gray_sync,
+	output reg [3:0]r_bi_ptr,
+	output reg [3:0]r_gray_ptr,
+	output reg rempty);
+
+reg [3:0]w_bi_sync;
+always@(*)begin
+	w_bi_sync[3]=w_gray_sync[3];
+	w_bi_sync[2]=w_bi_sync[3]^w_gray_sync[2];
+	w_bi-sync[1]=w_bi_sync[2]^w_gray_sync[1];
+	w_bi_sync[0]=w_bi_sync[1]^w_gray_sync[0];
+end
+
+always@(posedge rclk or negedge r_rst)begin
+	if(!r_rst)begin
+		r_bi_ptr<=4'd0;
+		r_gray_ptr<= 4'd0;
+		rempty<=1'b0;
+	end
+	else begin
+		if(r_en && !rempty)begin
+			r_bi_ptr<=r_bi_ptr+1;
+			r_gray_ptr<=((r_bi_ptr+1)>>1)^(r_bi_ptr+1);
+		end
+		if(r_bi_ptr==w_bi_sync)begin
+			rempty<=1'b1;
+		end
+		else begin
+			rempty<=1'b0;
+		end
+	end
+end
+endmodule
+
+//synchronizer
+module synchronizer(input clk,
+	input rst,
+	input [3:0]gray_ptr,
+	output reg[3:0]gray_sync);
+
+reg [3:0]sync_ff1;
+
+always@(posedge clk or negedge rst)begin
+	if(!rst)begin
+		sync_ff1<=4'd0;
+		gray_sync<=4'd0;
+	end
+	else begin
+		sync_ff1<=gray_ptr;
+		gray_sync<=sync_ff1;
+	end
+end
+endmodule
+
+
+//top module
+
+module async_fifo(input wclk,rclk,
+	input w_en,r_en,
+	input w_rst,r_rst,
+	input [7:0]wdata,
+	output reg [7:0]rdata,
+	output reg wfull,
+	output reg rempty);
+
+wire [3:0]w_bi_ptr;
+wire [3:0]w_gray_ptr;
+wire [3:0]w_gray_sync;
+
+wire [3:0]r_bi_ptr;
+wire [3:0]r_gray_ptr;
+wire [3:0]r_gray_sync;
+
+write_handler write(.wclk(wclk),
+	.w_rst(w_rst),
+	.w_en(w_en),
+	.r_gray_sync(r_gray_sync),
+	.w_bi_ptr(w_bi_ptr),
+	.w_gray_ptr(w_gray_ptr),
+	.wfull(wfull));
+
+read_handler read(.rclk(rclk),
+        .r_rst(r_rst),
+        .r_en(r_en),
+        .w_gray_sync(w_gray_sync),
+        .r_bi_ptr(r_bi_ptr),
+        .r_gray_ptr(r_gray_ptr),
+        .rempty(rempty));
+
+synchronizer sync_rptr(.clk(wclk),
+	.rst(w_rst),
+	.gray_ptr(r_gray_ptr),
+	.gray_sync(r_gray_sync));
+
+synchronizer sync_wptr(.clk(rclk),
+        .rst(r_rst),
+        .gray_ptr(w_gray_ptr),
+        .gray_sync(w_gray_sync));
+
+wire w_ena,r_ena;
+assign w_ena=w_en && !wfull;
+assign r_ena=r_en && !rempty;
+
+memory mem(.wclk(wclk),
+	.rclk(rclk),
+	.w_en(w_ena),
+	.w_addr(w_bi_ptr[2:0]),
+	.wdata(wdata),
+	.r_en(r_ena),
+	.r_addr(r_bi_ptr[2:0]),
+	.rdata(rdata));
+
+endmodule
+
+
+
+
+
+
